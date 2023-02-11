d4428c6405f7f80b19e821f679210c4cd3e76c17
unknown <dawidw22@gmail.com>
2023-02-11 13:17:55 +0100

    add: project config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      darkCyan: "hsl(158, 36%, 37%)",
+      cream: "hsl(30, 38%, 92%)",
+      veryDarkBlue: "hsl(212, 21%, 14%)",
+      darkGrayishBlue: "hsl(228, 12%, 48%)",
+    },
+    fontFamily: {
+      body: ["Montserrat", "sans-serif"],
+      heading: ["Fraunces", "serif"],
+    },
+    screens: {
+      sm: "480px",
+      md: "640px",
+      lg: "976px",
+      xl: "1440",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
